3bf14c2559b7b7df582803ec0c18a748104d099a
Pramendra Gupta <pramendra@gmail.com>
2020-06-16 14:57:42 +0900

    parse prismic query?

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,32 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import express from 'express';
-import { prismicClient } from './lib/prismic';
-const gql = require('graphql-tag');
+// const gql = require('graphql-tag');
+import { query } from './lib/prismic';
+import { lastPublishedQuery } from './gql/prismicQuery';
+
 const routeMain = express.Router({});
 
 routeMain.get('/', function (req: any, res: any) {
   res.status(200).send('Hello World!');
 });
 
-routeMain.post('/webhook', (req: any, res: any) => {
-  const {
-    PRISMIC_WEBHOOK_SECRET = '',
-    PRISMIC_GRAPHQL = '',
-    PRISMIC_ACCESS_TOKEN = '',
-  } = process.env;
+routeMain.post('/webhook', async (req: any, res: any) => {
+  const { PRISMIC_WEBHOOK_SECRET = '' } = process.env;
   const { secret = '' } = res.body || {};
   if (secret && secret !== PRISMIC_WEBHOOK_SECRET) {
     res.status(403).send('permission denied');
     return;
   }
+  // fetch last update timestamp
 
-  const client = prismicClient(PRISMIC_GRAPHQL, PRISMIC_ACCESS_TOKEN);
-  client
-    .query({
-      query: gql`
-        {
-          allNewss {
-            edges {
-              node {
-                _meta {
-                  id
-                  uid
-                  lastPublicationDate
-                }
-                hero
-                title
-                summary
-                references {
-                  title1
-                  src {
-                    _linkType
-                    __typename
-                    ... on _ExternalLink {
-                      url
-                    }
-                  }
-                }
-                category
-                tags {
-                  tag
-                }
-                rank
-              }
-            }
-          }
-        }
-      `,
-    })
-    .then((response: any) => {
-      console.log(response);
-    })
-    .catch((error: any) => {
-      console.error(error);
-    });
+  // create timestamp
+  const { totalCount = 0, edges = [] } = await query(lastPublishedQuery, {
+    lastPublicationDate_after: '2020-06-13T02:26:27+0000',
+  });
+  console.log(totalCount, edges);
+  // send notification
+  // post to slack
+  // update last update timestamp
 
   res.status(200).send('webhook');
 });
--- src/lib/prismic.ts
+++ src/lib/prismic.ts
@@ -12,3 +12,31 @@ export const prismicClient = (uri: string, accessToken: string): any => {
   });
   return client;
 };
+
+export const query = async (
+  query: any = '',
+  variables = {},
+  options = { fetchPolicy: 'no-cache' }
+): Promise<any> => {
+  try {
+    const { PRISMIC_GRAPHQL = '', PRISMIC_ACCESS_TOKEN = '' } = process.env;
+    const client = prismicClient(PRISMIC_GRAPHQL, PRISMIC_ACCESS_TOKEN);
+
+    const { data = {} } = await client.query({
+      query,
+      variables,
+      ...options,
+    });
+
+    if (data && data._allDocuments && data._allDocuments.totalCount) {
+      return {
+        totalCount: data._allDocuments.totalCount,
+        edges: data._allDocuments.edges,
+      };
+    }
+    return {};
+  } catch (error) {
+    console.warn('query', query.toString(), error.toString());
+    return {};
+  }
+};
